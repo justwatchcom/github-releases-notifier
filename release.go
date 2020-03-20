2be690d06074b40fc0d0d9c85318b284708dddb2
marthjod@gmail.com <marthjod@gmail.com>
2020-03-20 11:20:29 +0100

    optionally exclude non-stable versions

--- release.go
+++ release.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -12,4 +13,19 @@ type Release struct {
 	Description string
 	URL         url.URL
 	PublishedAt time.Time
+}
+
+// IsReleaseCandidate returns true if the release name hints at an RC release.
+func (r Release) IsReleaseCandidate() bool {
+	return strings.Contains(strings.ToLower(r.Name), "-rc")
+}
+
+// IsBeta returns true if the release name hints at a beta version release.
+func (r Release) IsBeta() bool {
+	return strings.Contains(strings.ToLower(r.Name), "beta")
+}
+
+// IsNonstable returns true if one of the non-stable release-checking functions return true.
+func (r Release) IsNonstable() bool {
+	return r.IsReleaseCandidate() || r.IsBeta()
 }
